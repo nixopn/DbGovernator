bfec4b33d4fb645cd3563a6024a9e131f29ee512
nixopn <eisnersandy2@gmail.com>
2026-08-07 17:09:56 +0300

    More tests

---
+++ Tests/TestCommand.cs
@@ -0,0 +1,43 @@
+﻿using DbGovernator.Abstractions;
+using DbGovernator.NDbClasses;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    [TestClass]
+    public sealed class TestCommand
+    {
+        private Mock<DbCommand> _innerCommand;
+        private Mock<ILogger> _logger;
+        private List<IVisitor> _visitors;
+        private Mock<IVisitor> _visitor;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _innerCommand = new Mock<DbCommand>();
+            _logger = new Mock<ILogger>();
+            _visitor = new Mock<IVisitor>();
+            _visitors = new List<IVisitor>() { _visitor.Object };
+        }
+
+        [TestMethod]
+        public void SetLoggerForVisitors()
+        {
+            var cmd = new NDbCommand(_innerCommand.Object, _visitors, _logger.Object);
+            _visitor.VerifySet(x => x.Logger = _logger.Object);
+        }
+
+        [TestMethod]
+        public void AllVisitorsCalled()
+        {
+            Thread.Sleep(22);
+        }
+    }
+}
---
+++ Tests/TestCommandAccessChecker.cs
@@ -0,0 +1,61 @@
+﻿using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DbGovernator;
+using DbGovernator.Abstractions;
+using DbGovernator.Realisations;
+using System.Data.Common;
+using Npgsql;
+
+namespace Tests
+{
+    [TestClass]
+    public sealed class TestCommandAccessChecker
+    {
+        private AccessChecker _accessChecker;
+        private Mock<ILogger> _logger;
+        private DbCommand _innerCommand;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _logger = new Mock<ILogger>();
+            _accessChecker = new AccessChecker();
+            _accessChecker.Logger = _logger.Object;
+            _innerCommand = new NpgsqlCommand();
+        }
+
+        [TestMethod]
+        public void AccessGranted()
+        {
+            _innerCommand.CommandText = "insert into users(name) values ('aaaaaaaa');";
+            var context = new DbGovernator.ExecutionContext();
+            context.QueryType = "insert";
+            context.Command = _innerCommand;
+            var step = new PrepareCommand();
+            step.Context = context;
+            _accessChecker.VisitPreparing(step);
+            Assert.AreEqual("insert into users(name) values ('aaaaaaaa');", _innerCommand.CommandText);
+        }
+
+        [TestMethod]
+        public void AccessNotGranted()
+        {
+            _innerCommand.CommandText = "delete from users where id = 299;";
+            var context = new DbGovernator.ExecutionContext();
+            context.QueryType = "delete";
+            context.TableName = "users";
+            context.Command = _innerCommand;
+            var step = new PrepareCommand();
+            step.Context = context;
+            var ex = Assert.ThrowsException<Exception>(() =>
+            {
+                _accessChecker.VisitPreparing(step);
+            });
+            Assert.AreEqual("Access denied", ex.Message);
+        }
+    }
+}
